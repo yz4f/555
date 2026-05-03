850025d38c2fd9d7bc9608798a150f9df2ddd1e4
yz4f <yasemoh24@gmail.com>
2026-05-03 05:47:03 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: "./",
+  server: {
+    host: "::",
+    port: 8080,
+    hmr: {
+      overlay: false,
+    },
+  },
+  plugins: [react(), mode === "development" && componentTagger(), viteSingleFile()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
+  },
+}));
